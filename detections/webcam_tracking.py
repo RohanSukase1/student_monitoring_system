a48fc979044f782efb6816af8dd17331016a1dbb
Rohan Sukase <rohansukase1@gmail.com>
2025-12-26 20:21:11 +0530

    committed webcam sample code

---
+++ detections/webcam_tracking.py
@@ -0,0 +1,20 @@
+import cv2
+from ultralytics import YOLO
+capture = cv2.VideoCapture(0)
+model=YOLO("D:/Projects/student_monitoring_system/models/id_card_model.pt")
+#model = YOLO("C:/Users/lenovo/Desktop/Programming/Datasets/Dataset_2/runs/detect/train3/weights/best.pt")
+
+while True:
+    isAvailable ,frame = capture.read()
+    if isAvailable :
+       result = model(source=frame,conf=0.10)
+       annotation = result[0].plot()
+       cv2.imshow("Live Tracking ",annotation)
+    else:
+       print("Something went wrong")
+       break
+    if cv2.waitKey(1) == ord("q") :
+       print("Quitting")
+       break
+capture.release()
+cv2.destroyAllWindows()
