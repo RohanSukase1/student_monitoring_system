db4493ddb5ecf79c9f2c12d27c0a720d10e19b44
Rohan Sukase <rohansukase1@gmail.com>
2025-12-27 19:46:09 +0530

    tracking code commit

--- detections/webcam_tracking.py
+++ detections/webcam_tracking.py
@@ -4,6 +4,7 @@ capture = cv2.VideoCapture(0)
 model=YOLO("D:/Projects/student_monitoring_system/models/id_card_model.pt")
 #model = YOLO("C:/Users/lenovo/Desktop/Programming/Datasets/Dataset_2/runs/detect/train3/weights/best.pt")
 
+
 while True:
     isAvailable ,frame = capture.read()
     if isAvailable :
